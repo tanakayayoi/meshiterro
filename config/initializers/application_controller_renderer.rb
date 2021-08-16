05ddbc6c2b1030fedeeb1e318412c9a9e700aa47
tanakayayoi <march.yayoi31@gmail.com>
2021-08-16 05:09:29 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd423ea8eb44cc2374c698593685a43daecb0ad2cf46294a438f0e6c042eed81851b7c9e59a769be45482d157ea05b1e2763e35580ebc023fab950f5d082a5f00'
